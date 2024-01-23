5db6e1568c6a6a9924e1965fdf46aa2274386021
seniorua <napishy1@.gmail.com>
2024-01-23 02:26:46 +0100

    nigh

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -2,10 +2,12 @@ export const Author = (props) => {
   
   return (
     <div className="author-item">
-      <h4>{props.title}</h4>
-      <h5>{props.subTitle}</h5>
-      <img className="author-img" src={props.pic} alt="Фото автора" />
-      <p>{props.text}</p>
+      <div className="author-item-inner">
+        <h3>{props.title}</h3>
+        <h5>{props.subTitle}</h5>
+        <img className="author-img" src={props.pic} alt="Фото автора" />
+        <p>{props.text}</p>
+      </div>
       <button>{props.btntxt}</button>
     </div>
   )
